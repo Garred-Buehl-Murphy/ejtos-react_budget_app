a6e15f74af3021b33b3ddfb0d59b4168ab5eee57
Garred-Buehl-Murphy <garredmurphywork@gmail.com>
2023-05-14 09:56:59 -0400

    modified the select to make it look as correct as possible in my browsers

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -10,14 +10,20 @@
         }
         
       return (
-            <div className='alert alert-secondary'> Currency {
-          <select name="Location" id="Location" value = {currency} onChange={event=>changeCurrency(event.target.value)} style={{color: 'white', backgroundColor: 'lightgreen' }}>
+            <div className='alert alert-secondary'> {
+          /*<select name="Location" id="Location" value = {currency} onChange={event=>changeCurrency(event.target.value)} style={{color: 'white', backgroundColor: 'lightgreen' }}>
+            <option value="$" label="Currency ($ Dolar)" style={{color: 'black'}}>$ Dolar</option>
+            <option value="£" label="Currency (£ Pound)" style={{color: 'black'}}>£ Pound</option>
+            <option value="€" label="Currency (€ Euro)" style={{color: 'black'}}>€ Euro</option>
+            <option value="₹" label="Currency (₹ Ruppee)" style={{color: 'black'}}>₹ Ruppee</option>
+          </select>*/
+          <span style={{color: 'red'}}>the html option label attribute does not fuction correctly in any of my browsers.<br/>               
+          <span style={{color: 'white', backgroundColor: 'lightgreen' }}> Currency (<select style={{color: 'white', backgroundColor: 'lightgreen' }} name="Location" id="Location" value = {currency} onChange={event=>changeCurrency(event.target.value)}>
             <option value="$" style={{color: 'black'}}>$ Dolar</option>
             <option value="£" style={{color: 'black'}}>£ Pound</option>
             <option value="€" style={{color: 'black'}}>€ Euro</option>
             <option value="₹" style={{color: 'black'}}>₹ Ruppee</option>
-
-          </select>	
+          </select>)</span></span>
           }	
         </div>
         );
